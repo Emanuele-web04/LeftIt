07d4c244a7a9a27aca68b353f880042b6537dcc4
Emanuele-web04 <emanueledipietro612@gmail.com>
2024-03-20 15:43:47 +0100

    Initial commit

---
+++ LeftIt/LeftItApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeftItApp.swift
+//  LeftIt
+//
+//  Created by Emanuele Di Pietro on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LeftItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
